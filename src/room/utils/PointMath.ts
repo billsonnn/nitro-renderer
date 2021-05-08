67eb04635fab1a571c6a9ce6710e986790187714
Dank074 <torresefrain10@gmail.com>
2021-05-08 18:29:07 -0500

    PointMath cleanup

--- src/room/utils/PointMath.ts
+++ src/room/utils/PointMath.ts
@@ -7,12 +7,12 @@ export class PointMath
         return new Point((k.x + _arg_2.x), (k.y + _arg_2.y));
     }
 
-    public static _Str_15193(k: Point, _arg_2: Point): Point
+    public static sub(k: Point, _arg_2: Point): Point
     {
         return new Point((k.x - _arg_2.x), (k.y - _arg_2.y));
     }
 
-    public static _Str_6038(k: Point, _arg_2: number): Point
+    public static mul(k: Point, _arg_2: number): Point
     {
         return new Point((k.x * _arg_2), (k.y * _arg_2));
     }
