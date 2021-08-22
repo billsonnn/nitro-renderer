b893a9d9b11767d34d84f206921bb2647b87ade8
dank074 <torresefrain10@gmail.com>
2021-08-22 03:16:14 -0500

    my life is a lie

--- src/nitro/communication/messages/incoming/sound/PlayListMessageEvent.ts
+++ src/nitro/communication/messages/incoming/sound/PlayListMessageEvent.ts
@@ -1,4 +1,5 @@
-import { IMessageEvent, MessageEvent } from '../../../../../core';
+import { IMessageEvent } from '../../../../../core/communication/messages/IMessageEvent';
+import { MessageEvent } from '../../../../../core/communication/messages/MessageEvent';
 import { PlayListMessageParser } from '../../parser';
 
 export class PlayListMessageEvent extends MessageEvent implements IMessageEvent
--- src/nitro/communication/messages/incoming/sound/PlayListSongAddedMessageEvent.ts
+++ src/nitro/communication/messages/incoming/sound/PlayListSongAddedMessageEvent.ts
@@ -1,4 +1,5 @@
-import { IMessageEvent, MessageEvent } from '../../../../../core';
+import { IMessageEvent } from '../../../../../core/communication/messages/IMessageEvent';
+import { MessageEvent } from '../../../../../core/communication/messages/MessageEvent';
 import { PlayListSongAddedMessageParser } from '../../parser';
 
 export class PlayListSongAddedMessageEvent extends MessageEvent implements IMessageEvent
