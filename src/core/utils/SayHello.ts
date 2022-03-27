01ba404d2b5fc80fbd0b1b67855e735f7a3204bc
Bill <billsonnn@live.com>
2022-03-27 19:58:51 -0400

    Update discord link

--- src/core/utils/SayHello.ts
+++ src/core/utils/SayHello.ts
@@ -5,7 +5,7 @@ export const SayHello = () =>
     if(navigator.userAgent.toLowerCase().indexOf('chrome') > -1)
     {
         const args = [
-            `\n %c %c %c Nitro ${ NitroVersion.UI_VERSION } - Renderer ${ NitroVersion.RENDERER_VERSION } %c %c %c https://nitrots.co/discord %c %c \n\n`,
+            `\n %c %c %c Nitro ${ NitroVersion.UI_VERSION } - Renderer ${ NitroVersion.RENDERER_VERSION } %c %c %c https://discord.nitrodev.co %c %c \n\n`,
             'background: #ffffff; padding:5px 0;',
             'background: #ffffff; padding:5px 0;',
             'color: #ffffff; background: #000000; padding:5px 0;',
