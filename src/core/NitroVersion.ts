bfc69c11f5b7f4f9af8498b26b6e10f8e5d8253f
Bill <billsonnn@live.com>
2022-04-11 12:20:01 -0400

    Bump version

--- src/core/NitroVersion.ts
+++ src/core/NitroVersion.ts
@@ -1,5 +1,5 @@
 export class NitroVersion
 {
-    public static RENDERER_VERSION: string = '1.2.0';
+    public static RENDERER_VERSION: string = '1.2.1';
     public static UI_VERSION: string = '';
 }
