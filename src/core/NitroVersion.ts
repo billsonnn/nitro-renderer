a495deff38e4f0e128315af2dd220974fe645495
Bill <billsonnn@live.com>
2022-11-09 17:38:38 -0500

    Version 1.4.4

--- src/core/NitroVersion.ts
+++ src/core/NitroVersion.ts
@@ -1,5 +1,5 @@
 export class NitroVersion
 {
-    public static RENDERER_VERSION: string = '1.4.3';
+    public static RENDERER_VERSION: string = '1.4.4';
     public static UI_VERSION: string = '';
 }
