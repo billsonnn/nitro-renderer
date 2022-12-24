9c139b442b16a76281edafca78c0bd8968df655a
oobjectt <objectholos@gmail.com>
2022-12-24 15:30:19 +0100

    #9 - TalentLevelUpEvent addd

--- src/nitro/communication/messages/incoming/talent/index.ts
+++ src/nitro/communication/messages/incoming/talent/index.ts
@@ -1,1 +1,2 @@
+export * from './TalentLevelUpEvent';
 export * from './TalentTrackMessageEvent';
--- src/nitro/communication/messages/parser/talent/index.ts
+++ src/nitro/communication/messages/parser/talent/index.ts
@@ -1,4 +1,6 @@
+export * from './TalentLevelUpMessageParser';
 export * from './TalentTrackLevel';
 export * from './TalentTrackParser';
+export * from './TalentTrackRewardPerk';
 export * from './TalentTrackRewardProduct';
 export * from './TalentTrackTask';
