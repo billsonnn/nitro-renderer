14831fb6bf6567dc150967820e553ce719326dec
oobjectt <objectholos@gmail.com>
2022-12-25 20:43:25 +0100

    #28 - RentableSpaceRentFailedMessageEvent added

--- src/nitro/communication/messages/incoming/room/furniture/index.ts
+++ src/nitro/communication/messages/incoming/room/furniture/index.ts
@@ -10,6 +10,7 @@ export * from './LoveLockFurniFinishedEvent';
 export * from './LoveLockFurniFriendConfirmedEvent';
 export * from './LoveLockFurniStartEvent';
 export * from './OneWayDoorStatusMessageEvent';
+export * from './RentableSpaceRentFailedMessageEvent';
 export * from './RequestSpamWallPostItMessageEvent';
 export * from './RoomDimmerPresetsMessageEvent';
 export * from './wall';
