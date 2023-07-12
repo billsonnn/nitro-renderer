f701f977e3b6036acfb255f5325d00bedbbb4a8d
Bill <billsonnn@live.com>
2023-07-11 21:55:51 -0400

    Remove useless plugin

--- src/nitro/Plugins.ts
+++ src/nitro/Plugins.ts
@@ -3,7 +3,6 @@ import { BatchRenderer, extensions } from '@pixi/core';
 import { Extract } from '@pixi/extract';
 import '@pixi/graphics-extras';
 import { InteractionManager } from '@pixi/interaction';
-import { AppLoaderPlugin } from '@pixi/loaders';
 import '@pixi/math-extras';
 import '@pixi/mixin-cache-as-bitmap';
 import '@pixi/mixin-get-child-by-name';
@@ -19,5 +18,4 @@ extensions.add(
     TilingSpriteRenderer,
     InteractionManager,
     SpritesheetLoader,
-    AppLoaderPlugin,
     TickerPlugin);
