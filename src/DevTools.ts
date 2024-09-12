afe14eafe9d6c18e7099883ed95b6ecde5a8002c
Bill <billsonnn@billsonnn.com>
2024-09-12 18:09:38 -0400

    Update DevTools

--- src/DevTools.ts
+++ src/DevTools.ts
@@ -1,6 +1,4 @@
 import { GetRoomEngine, RoomEngine } from '@nitrots/room';
-import { GetRenderer, GetTexturePool } from '@nitrots/utils';
-import { Texture, TextureGCSystem, TextureSource } from 'pixi.js';
 export { };
 
 declare global
@@ -10,16 +8,10 @@ declare global
 		NitroDevTools?:
 		{
             roomEngine: RoomEngine;
-			textureCache: TextureSource<any>[];
-			texturePool: { [index: string]: { [index: string]: Texture[] } };
-			textureGC: TextureGCSystem;
 		};
 	}
 }
 
 window.NitroDevTools = {
-    roomEngine: GetRoomEngine(),
-    textureCache: GetRenderer().texture.managedTextures,
-    texturePool: GetTexturePool().textures,
-    textureGC: GetRenderer().textureGC
+    roomEngine: GetRoomEngine()
 };
